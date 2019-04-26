ea6929126203bf909e5bd8ac2d136cfcefc11808
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2019-04-26 12:16:33 +0200

    fix: export pState

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import { pDelay } from './pDelay'
 import { pHang } from './pHang'
 import { pMap } from './pMap'
 import { pProps } from './pProps'
+import { pState } from './pState'
 
-export { pMap, pProps, pDelay, pDefer, pHang }
+export { pMap, pProps, pDelay, pDefer, pHang, pState }
